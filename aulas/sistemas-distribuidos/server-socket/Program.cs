3bf5b868c9a05bb43ce42d5189988929d423f33f
Erico de Souza Loewe <ed-lowe@hotmail.com>
2019-08-06 22:03:09 -0300

    adicionar servidor

---
+++ aulas/sistemas-distribuidos/server-socket/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace server_socket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // Establish the local endpoint for the socket.  
+            // The DNS name of the computer  
+            // running the listener is "host.contoso.com".  
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 8080);
+
+            Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(100);
+                listener.Accept();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\nPress ENTER to continue...");
+        }
+    }
+}
